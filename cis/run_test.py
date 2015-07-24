de71390754fbd00d8e214b0b74eb3bb062a98692
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2015-07-24 09:46:05 +0100

    The anaconda build environment doesn't like finding the tests using the path syntax for nose so I've explicitly called the unit tests using the relative import syntax instead.

--- cis/run_test.py
+++ cis/run_test.py
@@ -2,9 +2,9 @@ from cis.test.runner import run
 import os
 
 # Disable multi proccesing on windows as it doesn't appear to work
-np = 0 if os.name == 'nt' else np = 1
+np = 0 if os.name == 'nt' else 1
 # Run the tests
-run(n_processors=np)
+run('cis.test.unit', n_processors=np)
 
 # If the test data directory is specified, run the integration tests as well. We disable parallel processing
 # of these though as some have side affects, you also have to specify a time-out which would have to be very large
